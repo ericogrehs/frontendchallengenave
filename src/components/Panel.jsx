f5cc4290b2a377ae80573bb29cc532d0e648843f
Erico Grehs <ericogrehs@gmail.com>
2019-09-23 03:33:02 -0300

    UserTable development
    - Added material-ui for sophisticated design of Table and Table content.
    - Added axios.
    - Fetching users data when component UserTable mount.

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
-import Table from './Table';
+import UserTable from './UserTable';
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     background-color: #F0F0F0;
     flex: 1;
+    padding: 24px;
+    @media only screen and (min-width: 1024px) {
+        padding: 32px;
+    }
 `;
 
 const ContainerMenu = styled.div`
@@ -17,14 +22,13 @@ const ContainerMenu = styled.div`
 `;
 
 export default function Panel() {
-
     return (
         <Container>
             <ContainerMenu>
                 <h1>Title</h1>
                 <button>Click</button>
             </ContainerMenu>
-            <Table />
+            <UserTable />
         </Container>
     );
 }
